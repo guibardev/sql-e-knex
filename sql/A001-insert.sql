77d54e4d6f1170ef741b398fbfd506a6c3f13be3
guibardev <guibardevs@gmail.com>
2025-04-21 18:59:29 -0300

    Update A001-insert.sql

--- sql/A001-insert.sql
+++ sql/A001-insert.sql
@@ -10,3 +10,17 @@ insert into users
 ("Helena", "A.", "1@email.com", "3_hash"),
 ("Joana", "B.", "2@email.com", "4_hash"),
 ("Rosana", "C.", "3@email.com", "5_hash");
+
+
+
+("24",	"Banco de Dados"),
+("1",	"Bruno"),
+("23",	"Desenvolvimento Web"),
+("31",	"Gestão de Projetos Ágeis"),
+("29",	"Inteligência Artificial"),
+("28",	"Introdução à Segurança da Informação"),
+("32",	"Lógica Matemática"),
+("25",	"Machine Learning"),
+("27",	"Mobile Development"),
+("26",	"Python Avançado"),
+("30",	"UX Design");
